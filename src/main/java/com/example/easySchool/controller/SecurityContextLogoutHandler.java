7e9f1dfded7f438919d2d82117160a5a9f2c09de
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-07-15 23:29:59 +0530

    added spring security Handler

---
+++ src/main/java/com/example/easySchool/controller/SecurityContextLogoutHandler.java
@@ -0,0 +1,20 @@
+package com.example.easySchool.controller;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.util.Assert;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecurityContextLogoutHandler {
+
+
+    private boolean invalidateHttpSession = true;
+
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication){
+        Assert.notNull(request,"HttpServletRequest required");
+        if(this.invalidateHttpSession){
+
+        }
+    }
+}
