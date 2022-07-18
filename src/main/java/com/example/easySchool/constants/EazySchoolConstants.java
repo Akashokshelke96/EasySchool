5b1d4de06950464e14a275526efdbb22df2c0074
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-07-18 23:35:54 +0530

    added schema.sql & messages

---
+++ src/main/java/com/example/easySchool/constants/EazySchoolConstants.java
@@ -0,0 +1,10 @@
+package com.example.easySchool.constants;
+
+public class EazySchoolConstants {
+
+    public static final String ANONYMOUS = "Anonymous";
+    public static final String OPEN = "Open";
+    public static final String CLOSE = "Close";
+
+}
+
