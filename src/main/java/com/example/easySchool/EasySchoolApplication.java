162a47b4c927da590e11e57b13e6b32502c0bf45
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-07-20 23:18:46 +0530

    added corrections

--- src/main/java/com/example/easySchool/EasySchoolApplication.java
+++ src/main/java/com/example/easySchool/EasySchoolApplication.java
@@ -2,9 +2,13 @@ package com.example.easySchool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.easySchool.repository")
+@EntityScan("com.example.easySchool.model")
 public class EasySchoolApplication {
 
 	public static void main(String[] args) {
