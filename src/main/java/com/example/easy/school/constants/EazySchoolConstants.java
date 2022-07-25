d9af68659bdf66b202697d6b5e2ea1f453e75f5f
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-07-25 23:38:27 +0530

    added easy school New user registration

--- src/main/java/com/example/easy/school/constants/EazySchoolConstants.java
+++ src/main/java/com/example/easy/school/constants/EazySchoolConstants.java
@@ -1,10 +1,13 @@
 package com.example.easy.school.constants;
 
+import com.example.easy.school.models.Roles;
+
 public class EazySchoolConstants {
 
-    public static final String ANONYMOUS = "Anonymous";
+
     public static final String OPEN = "Open";
     public static final String CLOSE = "Close";
-
+    public static final String STUDENT_ROLE = "STUDENT";
+    public static final String ADMIN_ROLE = "ADMIN";
 }
 
